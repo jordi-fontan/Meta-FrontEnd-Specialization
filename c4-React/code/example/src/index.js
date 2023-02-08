847867acb64c777a00909a0b592c6afc174a6acc
jordi-fontan <63612112+jordi-fontan@users.noreply.github.com>
2023-02-08 18:25:43 +0100

    Add module1 exam + lab

--- c4-React/code/example/src/index.js
+++ c4-React/code/example/src/index.js
@@ -7,8 +7,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App >
-    <p>Hello World</p></App>
+    <App />
   </React.StrictMode>
 );
 
