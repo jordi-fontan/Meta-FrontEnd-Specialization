e2494931868ed90e082c30ffaabf5b37ffbf2ec0
jordi-fontan <63612112+jordi-fontan@users.noreply.github.com>
2023-05-15 18:28:00 +0200

    add autostyling a component

---
+++ c5-React/code/6-InternalStyling/internal-styling/src/App.js
@@ -0,0 +1,16 @@
+function App() {
+
+const appStyle={
+  background:"pink",
+  width:"calc(30%-10px)",
+  marginLeft: "10px"
+}
+
+  return (
+    <div style={appStyle}>
+      Hello word
+    </div>
+  );
+}
+
+export default App;
