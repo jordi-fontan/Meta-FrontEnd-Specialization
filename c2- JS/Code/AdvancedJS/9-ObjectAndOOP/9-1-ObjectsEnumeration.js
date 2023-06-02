9833cf1a43513dcd425d543c9c64f13f511262e7
jordi-fontan <63612112+jordi-fontan@users.noreply.github.com>
2023-06-02 23:37:29 +0200

    Add object keys and delete

--- c2- JS/Code/AdvancedJS/9-ObjectAndOOP/9-1-ObjectsEnumeration.js
+++ c2- JS/Code/AdvancedJS/9-ObjectAndOOP/9-1-ObjectsEnumeration.js
@@ -18,3 +18,17 @@ console.log(sam1);
 //But 
 const sam2 ={name:'Sam',age:4};
 console.log(sam2);
+
+sam2.speak=function () {return 'bla bla'};
+sam2.speak();
+
+//sam1.speak(); //Reference Error
+
+delete (sam2.age);
+console.log(sam2);
+
+delete(sam1);
+
+Object.keys(sam1).forEach(element => {
+    console.log(`${element} ${sam2[element]}` );
+});
