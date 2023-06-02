c9702c5879d2cac578162065a7fae10c4511b88d
jordi-fontan <63612112+jordi-fontan@users.noreply.github.com>
2023-06-02 22:56:07 +0200

    Add self fix to inner function

--- c2- JS/Code/AdvancedJS/9-ObjectAndOOP/ObjectLiteralsAndThis.js
+++ c2- JS/Code/AdvancedJS/9-ObjectAndOOP/ObjectLiteralsAndThis.js
@@ -1,6 +1,36 @@
-var literalCircle = {
+var literalCircle = { // New Object()
     radius:20,
     getArea: function(){
+        console.log(this);
+        return Math.PI*Math.pow(this.radius,2);
+    }
+};
+console.log(literalCircle.getArea());
+
+delete literalCircle;
+//let's add ain inner funtion and work with self
+
+
+ literalCircle = { // New Object()
+    radius:20,
+    getArea: function(){
+        this.radius=40; // wont work inEC5
+        console.log(this);
+        return Math.PI*Math.pow(this.radius,2);
+    }
+};
+console.log(literalCircle.getArea());
+
+delete literalCircle;
+//let's add ain inner funtion and work with self
+
+
+ literalCircle = { // New Object()
+    
+    radius:20,
+    getArea: function(){
+       var self=this;
+        self.radius=1000; 
         console.log(this);
         return Math.PI*Math.pow(this.radius,2);
     }
